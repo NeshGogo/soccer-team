45815cdd32f13c6ae78d9e0b21b9a782563e3974
NeshGogo <rafaelaguero10@hotmail.com>
2020-07-20 16:28:00 -0400

    aplicando estilos para player dialog y creando logica para editar jugadores

--- src/app/player-dialog/player-dialog.component.ts
+++ src/app/player-dialog/player-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Countries, SquadNumber } from '../interfaces/player';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Countries, SquadNumber, Player } from '../interfaces/player';
 import { PlayerService } from '../services/player.service';
 import { TeamService } from '../services/team.service';
 import { take } from 'rxjs/operators';
@@ -12,15 +12,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./player-dialog.component.scss'],
 })
 export class PlayerDialogComponent implements OnInit {
+
+  @Input() player: Player;
+  @Output() closeDialog: EventEmitter<boolean> = new EventEmitter();
   private team: Team;
   public countries = Object.keys(Countries).map((key) => ({
     label: key,
     key: Countries[key],
   }));
-  public squadNumber = Object.keys(SquadNumber).map((key) => ({
-    label: key,
-    key: Countries[key],
-  }));
+
+  public squadNumber = Object.keys(SquadNumber)
+    .slice(Object.keys(SquadNumber).length / 2)
+    .map((key) => ({
+      label: key,
+      key: SquadNumber[key],
+    }));
 
   constructor(
     private playerService: PlayerService,
@@ -44,13 +50,41 @@ export class PlayerDialogComponent implements OnInit {
       ...playerFormValue,
       key,
     };
+    const players = this.team.players.length > 0 ? this.team.players : [];
+    console.log(players);
+    const formattedTeam = {
+      ...this.team,
+      players: [...players, playerFormValueKey],
+    };
+    console.log(formattedTeam);
+    this.teamService.editTeam(formattedTeam);
+  }
+
+  private editPlayer(playerFormValue): void {
+    const playerFormValueWithKey = {
+      ...playerFormValue,
+      $key: this.player.$key,
+    };
+    const playerFormValueWithFormattedKey = {
+      ...playerFormValue,
+      key: this.player.$key,
+    };
+    const moddifiedPlayers = this.team.players
+      ? this.team.players.map((player) => {
+          return player.key === this.player.$key
+            ? playerFormValueWithFormattedKey
+            : player;
+        })
+      : this.team.players;
     const formattedTeam = {
       ...this.team,
       players: [
-        ...(this.team.players ? this.team.players : []),
-        playerFormValueKey,
+        ...(moddifiedPlayers
+          ? moddifiedPlayers
+          : [playerFormValueWithFormattedKey]),
       ],
     };
+    this.playerService.editPlayer(playerFormValueWithKey);
     this.teamService.editTeam(formattedTeam);
   }
 
@@ -58,9 +92,21 @@ export class PlayerDialogComponent implements OnInit {
     const playerFormValue = { ...playerForm.value };
     if (playerForm.valid) {
       playerFormValue.leftFooted =
-        playerFormValue.leftFooted === '' ? false : true;
+        playerFormValue.leftFooted === '' ||
+        playerFormValue.leftFooted === undefined
+          ? false
+          : true;
     }
-    this.newPlayer(playerFormValue);
+    if (this.player) {
+      this.editPlayer(playerFormValue);
+    } else {
+      this.newPlayer(playerFormValue);
+    }
+    this.onClose();
     window.location.replace('#');
+  }
+
+  onClose(): void {
+    this.closeDialog.emit(true);
   }
 }
